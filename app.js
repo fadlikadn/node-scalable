1434bc9c7b71b92060207f0d44f55c0e8a8f8bfb
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-12-06 21:35:26 +0700

    User middleware body-parser to handle body request

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 let express = require("express");
+let body = require("body-parser");
+let route = express.Router();
 let app = express();
 let stack = [];
 
-app.post("/stack", (req, res, next) => {
-    let buffer = "";
+app.use(body.text({ type: "*/*" }));
 
-    req.on("data", (data) => {
-        buffer += data;
-    });
-    req.on("end", () => {
-        stack.push(buffer);
-        return next();
-    });
+route.post("/", (req, res, next) => {
+    stack.push(req.body);
+    return next();
 });
 
-app.delete("/stack", (req, res, next) => {
+route.delete("/", (req, res, next) => {
     stack.pop();
     return next();
 });
 
-app.get("/stack/:index", (req, res) => {
+route.get("/:index", (req, res) => {
     if (req.params.index >= 0 && req.params.index < stack.length) {
         return res.end("" + stack[req.params.index]);
     }
     res.status(404).end();
 });
 
-app.use("/stack", (req, res) => {
+route.use((req, res) => {
     res.send(stack);
 });
 
+app.use("/stack", route);
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
